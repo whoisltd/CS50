a941fc8e7e9cf3f12829505c068d6182ecfe99ea
WhoIsLTD <xuppermario@gmail.com>
2020-05-19 14:13:30 +0700

    update flask, application.py

--- python/application.py
+++ python/application.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-     return "Hello World!"
+     headline = "TinTuyenSinh"
+     return render_template("index.html", test= headline)
+@app.route("/ltd")
+def ltd():
+     return "Hello LTD!"
+@app.route("/<string:name>")
+def hello(name):
+     name = name.capitalize()
+     return "<h1>Hello, " + name + "</h1>"
+@app.route("/new_year")
+def new():
+     now = datetime.datetime.now()
+     new_year = now.month == 1 and now.day == 1
+     new_year = True
+     return render_template("index.html", new_year=new_year)
